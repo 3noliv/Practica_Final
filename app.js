d24154337a3aff33aef5765c572c54dce259892c
3noliv <enoliv02@gmail.com>
2025-04-21 19:32:52 +0200

    Slack hecho, ahora los errores aparecen en slack

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const setupSwagger = require("./config/swagger");
 const cors = require("cors");
 require("dotenv").config();
+const morganBody = require("morgan-body");
+const loggerStream = require("./utils/handleLogger");
 
 const dbConnect = require("./config/mongo");
 const routes = require("./routes");
@@ -11,6 +13,14 @@ setupSwagger(app);
 
 app.use(cors());
 app.use(express.json());
+
+morganBody(app, {
+  noColors: true,
+  skip: function (req, res) {
+    return res.statusCode < 400; // Solo errores
+  },
+  stream: loggerStream,
+});
 
 // Usar el index de rutas con prefijo /api
 app.use("/api", routes);
